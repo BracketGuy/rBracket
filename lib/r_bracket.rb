7942e9dfc240f80aba85e793cbaa636fb3e7e832
Marcus McDuffie <marcus.mcduffie.83@gmail.com>
2014-10-20 21:37:04 -0400

    First commit.

---
+++ lib/r_bracket.rb
@@ -0,0 +1,3 @@
+class RBracket
+  VERSION = "1.0.0"
+end
---
+++ test/test_r_bracket.rb
@@ -0,0 +1,9 @@
+gem "minitest"
+require "minitest/autorun"
+require "r_bracket"
+
+class TestRBracket < Minitest::Test
+  def test_sanity
+    flunk "write tests or I will kneecap you"
+  end
+end
