873390d8fab200faf34b069bf8345cf6ec54e424
Marcus McDuffie <marcus.mcduffie.83@gmail.com>
2014-10-25 10:46:43 -0400

    Making Ruby work like C may be folly, but here we go...

---
+++ lib/io.rb
@@ -0,0 +1,31 @@
+def handle_command_args
+    if ARGV.length == 0
+        puts "usage: bracket [filename]"
+    elsif ARGV.length == 1
+        return ARGV[0]
+    end
+end
+
+def count_lines_in_file(file)
+    count = file.readlines.length
+    file.rewind
+    return count
+end
+
+def read_in_file(filename)
+    if filename.nil?
+        return
+    else
+        begin
+            file = File.open(filename,"r")
+        rescue
+            puts "Could not open file #{filename}. Make sure the file exists."
+            return
+        end
+        count = count_lines_in_file(file)
+        file.each_char do |c|
+            print c
+        end
+        puts "Processed #{count} lines."
+    end
+end
