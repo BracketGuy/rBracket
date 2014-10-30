ce764b8408c97ef67658efea2cd450f273c81e48
Marcus McDuffie <marcus.mcduffie.83@gmail.com>
2014-10-30 19:09:57 -0400

    I think this is working. Now to do this in C...

--- lib/io.rb
+++ lib/io.rb
@@ -15,31 +15,39 @@ def count_lines_in_file(file)
     return count
 end
 
+def open_file_for_copy(filename)
+    begin
+        file = File.open(filename,"r")
+    rescue
+        puts "Could not open file #{filename}. Make sure the file exists."
+        exit
+    else
+        return file
+    end
+end
+
 def read_in_file(filename)
     if filename.nil?
         return
     else
-        begin
-            file = File.open(filename,"r")
-        rescue
-            puts "Could not open file #{filename}. Make sure the file exists."
-            return
-        end
+        file = open_file_for_copy(filename)
         count = count_lines_in_file(file)
         source_file = SourceFile.new(count)
-        char_buffer = []
+        char_buffer = "" 
         line_counter = 0
         file.each_char do |c|
             if c == "\n"
                 ++line_counter
                 source_file_line = SourceFileLine.new(line_counter,char_buffer)
                 source_file.add_line(source_file_line)
-                char_buffer = []
+                char_buffer = ""
             else
-                char_buffer.push(c)
+                char_buffer << c
             end
         end
-        puts source_file.lines
+        source_file.lines.each do |line|
+            puts line.line_text
+        end
         puts "Processed #{count} lines."
     end
 end
