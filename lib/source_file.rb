055ba0cdf7be2dd3a44abdbb52e4426642d064a0
Marcus McDuffie <marcus.mcduffie.83@gmail.com>
2014-11-20 15:47:12 -0500

    Removed useless tests.

--- lib/source_file.rb
+++ lib/source_file.rb
@@ -1,11 +1,6 @@
 class SourceFile
-    attr_accessor :total_lines, :lines
-    def initialize(total_lines)
-        @total_lines = total_lines
-        @lines = []
-    end
 
-    def add_line(source_file_line)
-        @lines.push(source_file_line)
+    def initialize()
+        
     end
-end 
+end
--- test/test_source_file.rb
+++ test/test_source_file.rb
@@ -4,20 +4,6 @@ require "source_file"
 class TestSourceFile < Minitest::Test
 
     def test_source_file_create
-        source_file = SourceFile.new(2)
-        assert_equal 2, source_file.total_lines
-    end
-
-    def test_source_file_line_create
-        source_file_line = SourceFileLine.new(2,"test line")
-        assert_equal source_file_line.line_number, 2
-        assert_equal "test line", source_file_line.text
-    end
-
-    def test_source_file_add_line
-        source_file = SourceFile.new(1)
-        source_file_line = SourceFileLine.new(1,"test line")
-        source_file.add_line(source_file_line)
-        assert_equal source_file_line, source_file.lines[0]
+        #TODO
     end
 end
