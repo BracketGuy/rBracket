410131beeb5cf02a39e3fc15f9d2a753f33055a4
Marcus McDuffie <marcus.mcduffie.83@gmail.com>
2014-11-21 10:43:53 -0500

    Improved SourceFile initializer.

--- lib/source_file.rb
+++ lib/source_file.rb
@@ -1,6 +1,30 @@
+require "source_file_char"
+
 class SourceFile
 
-    def initialize()
+    attr_accessor :source_chars
+
+    def initialize(source_text)
+        @source_text  = source_text.split(//)
+        @source_chars = []
         
+        line_counter = 1
+        pos = 1
+        @source_text.each do |char|
+            if char == "\n"
+                line_counter += 1
+                pos = 0 #Newline does not count as position.
+            end
+            new_char = SourceFileChar.new(char,line_counter,pos)
+            pos += 1
+            @source_chars.push(new_char)
+        end
+    end
+    
+    def show_source_chars
+        puts "\n"
+        @source_chars.each do |char|
+            puts "Value: #{char.value} Line: #{char.line_num} Position: #{char.pos}"
+        end
     end
 end
--- test/test_source_file.rb
+++ test/test_source_file.rb
@@ -4,6 +4,10 @@ require "source_file"
 class TestSourceFile < Minitest::Test
 
     def test_source_file_create
-        #TODO
+        source_file = SourceFile.new("[ foo : \n [ bar ] ]")
+        source_char = source_file.source_chars[2]
+        assert_equal "f", source_char.value
+        assert_equal 3, source_char.pos
+        assert_equal 1, source_char.line_num
     end
 end
